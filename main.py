8ef2965aa611dda2e9b93385d066dce9f6632ab5
Martin Jansen <martin.jansen@gmx.ch>
2021-11-03 17:20:55 +0100

    building url

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
-# This is a sample Python script.
+import urllib.request
+import datetime
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+# import eccodes
+# import bunzip2
+from datetime import date
+from typing import Type
 
 
-# Press the green button in the gutter to run the script.
+def download():
+    url = build_url()
+    urllib.request.urlretrieve(url, "download4.grib2.bz2")
+
+
+def build_url():
+    date = get_latest_model_date()
+    hour = get_latest_model_hour()
+    forecast_time = get_forecast_time()
+    modellevel = get_modellevel()
+    variable = get_variable()
+    url = f"https://opendata.dwd.de/weather/nwp/icon-d2/grib/{hour}/{variable}" \
+          f"/icon-d2_germany_regular-lat-lon_model-level_{date}{hour}_{forecast_time}_{modellevel}_{variable}.grib2.bz2"
+    return url
+
+
+
+def get_latest_model_date():
+    #date = datetime.isoformat
+    #print(date)
+    return "20211103"
+
+
+def get_latest_model_hour():
+    return "00"
+
+
+def get_forecast_time():
+    return "000"
+
+
+def get_modellevel():
+    return "1"
+
+def get_variable():
+    return "u"
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    download()
